efe313fbd0a6e2b3ced08b8eb7c2b2820b62dabb
adayoooooooo <keitohiga2000@gmail.com>
2026-06-21 06:16:48 +0900

    Update rayfield_ui.lua

--- rayfield_ui.lua
+++ rayfield_ui.lua
@@ -11,7 +11,7 @@ local Window = RayfieldLibrary:CreateWindow({
         FileName = "Config"
     }
 })
-window:Createtab("a")
+window:Createtab("a", 13585613929)
 local PlayerTab = Window:CreateTab("Player", 13585613929)
 
 PlayerTab:CreateToggle({
